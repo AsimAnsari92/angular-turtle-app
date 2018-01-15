1c8e9b86bdbf1da1678ca920db6717af02208904
shoaibansari <shoaibansari824@gmail.com>
2018-01-16 00:50:57 +0500

    update quiz start screen

--- js/controllers/QuizController.js
+++ js/controllers/QuizController.js
@@ -15,6 +15,35 @@ function QuizController(quizMectrics,DataService)
 	var vm = this;
 	vm.quizMectrics = quizMectrics;
 	vm.DataService = DataService;
+	vm.activeQuestion=	0;
+	vm.QuizAnswer= QuizAnswer;
+
+	var numberofQuestionAnswered= 0;
+	function QuizAnswer()
+	{
+		var quizLength = DataService.quizQuestions.length - 1;
+		if(quizLength<vm.activeQuestion)
+		{
+			vm.activeQuestion++;
+		}
+	}
+
+
+
+
+	function QuestionAnswer()
+	{
+		var quizLength = DataService.quizQuestions.length;
+		//console.log(DataService.quizQuestions[vm.activeQuestion].selected); 
+		if(DataService.quizQuestions[vm.activeQuestion].selected!==null)
+		{
+			numberofQuestionAnswered++;
+			if(numberofQuestionAnswered>=quizLength)
+			{
+
+			}
+		}
+	}
 
 }
 
